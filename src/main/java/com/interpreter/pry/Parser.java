0186589ac8584706ea88338e617a69d3efb8fe54
AlyElashram <71949028+AlyElashram@users.noreply.github.com>
2024-03-10 03:33:02 +0200

    Added while statement parsing to the parser

--- src/main/java/com/interpreter/pry/Parser.java
+++ src/main/java/com/interpreter/pry/Parser.java
@@ -195,6 +195,7 @@ public class Parser {
     private Stmt statement() {
         if (match(IF)) return ifStatement();
         if (match(PRINT)) return printStatement();
+        if (match(WHILE)) return whileStatement();
         if (match(LEFT_BRACE)) return new Stmt.Block(block());
         return expressionStatement();
     }
@@ -243,5 +244,13 @@ public class Parser {
         }
         consume(RIGHT_BRACE, "Expect '}' after block.");
         return statements;
+    }
+
+    private Stmt whileStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'while'.");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after condition.");
+        Stmt body = statement();
+        return new Stmt.While(condition, body);
     }
 }
