cb142a22be70ff71b99a53ee00ab6f5ca218391e
AlyElashram <71949028+AlyElashram@users.noreply.github.com>
2024-03-10 02:39:52 +0200

    Added Expr Classes and visitor methods for ternary, assign and logical operators

--- src/main/java/com/interpreter/pry/Expr.java
+++ src/main/java/com/interpreter/pry/Expr.java
@@ -1,18 +1,49 @@
 package com.interpreter.pry;
 
+import java.util.List;
 
 abstract class Expr {
  interface Visitor<R> {
+ R visitAssignExpr(Assign expr);
+ R visitTernaryExpr(Ternary expr);
  R visitBinaryExpr(Binary expr);
  R visitGroupingExpr(Grouping expr);
  R visitLiteralExpr(Literal expr);
- R visitTernaryExpr(Ternary expr);
+ R visitLogicalExpr(Logical expr);
  R visitUnaryExpr(Unary expr);
  R visitVariableExpr(Variable expr);
- R visitAssignExpr(Assign expr);
 
  }
- abstract <R> R accept(Visitor<R> visitor);
+ static class Assign extends Expr {
+ Assign(Token name, Expr value) {
+ this.name = name;
+ this.value = value;
+ }
+
+ @Override
+ <R> R accept(Visitor<R> visitor) {
+ return visitor.visitAssignExpr(this);
+ }
+
+ final Token name;
+ final Expr value;
+ }
+ static class Ternary extends Expr {
+ Ternary(Expr condition , Expr valid , Expr invalid) {
+ this.condition = condition;
+ this.valid = valid;
+ this.invalid = invalid;
+ }
+
+ @Override
+ <R> R accept(Visitor<R> visitor) {
+ return visitor.visitTernaryExpr(this);
+ }
+
+ final Expr condition ;
+ final Expr valid ;
+ final Expr invalid;
+ }
  static class Binary extends Expr {
  Binary(Expr left, Token operator, Expr right) {
  this.left = left;
@@ -53,6 +84,22 @@ abstract class Expr {
 
  final Object value;
  }
+ static class Logical extends Expr {
+ Logical(Expr left, Token operator, Expr right) {
+ this.left = left;
+ this.operator = operator;
+ this.right = right;
+ }
+
+ @Override
+ <R> R accept(Visitor<R> visitor) {
+ return visitor.visitLogicalExpr(this);
+ }
+
+ final Expr left;
+ final Token operator;
+ final Expr right;
+ }
  static class Unary extends Expr {
  Unary(Token operator, Expr right) {
  this.operator = operator;
@@ -67,23 +114,6 @@ abstract class Expr {
  final Token operator;
  final Expr right;
  }
- static class Ternary extends Expr {
-
-  final Expr condition;
-  final Expr valid;
-  final Expr invalid;
-
-  Ternary(Expr condition, Expr valid, Expr invalid) {
-   this.condition = condition;
-   this.valid = valid;
-   this.invalid = invalid;
-  }
-
-  @Override
-  <R> R accept(Visitor<R> visitor) {
-   return visitor.visitTernaryExpr(this);
-  }
- }
  static class Variable extends Expr {
  Variable(Token name) {
  this.name = name;
@@ -97,17 +127,5 @@ abstract class Expr {
  final Token name;
  }
 
- static class Assign extends Expr{
-  final Token name;
-  final Expr value;
-  Assign(Token name,Expr value){
-   this.name = name;
-   this.value = value;
-  }
-  @Override
-  <R> R accept(Visitor<R> visitor) {
-   return visitor.visitAssignExpr(this);
-  }
-
- }
+  abstract <R> R accept(Visitor<R> visitor);
 }
